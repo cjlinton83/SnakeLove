022499f81a64a0cc13c7b988f3d17d30180a19ef
Christopher Linton <cjlinton83@gmail.com>
2018-08-05 00:55:12 -0500

    luaunit tests

--- test/list.lua
+++ test/list.lua
@@ -1,68 +1,66 @@
 local init = function()
-    local list = {}
-
-    function list:setHead(x, y)
-        self.head = self:newNode(x, y)
-    end
-
-    function list:newNode(x, y)
-        local node = {}
-        node.x = x
-        node.y = y
-        return node
-    end
-
-    function list:findTail()
-        local current = self.head
-
-        if current == nil then
-            return nil
+    list = {}
+        function list:setHead(x, y)
+            self.head = self:newNode(x, y)
         end
 
-        while current.next ~= nil do
-            current = current.next
+        function list:newNode(x, y)
+            local node = {}
+            node.x = x
+            node.y = y
+            return node
         end
 
-        return current
-    end
+        function list:findTail()
+            local current = self.head
 
-    function list:popTail()
-        local previous = self.head
-        local current = previous
+            if current == nil then
+                return nil
+            end
 
-        while current.next ~= nil do
-            previous = current
-            current = current.next
+            while current.next ~= nil do
+                current = current.next
+            end
+
+            return current
         end
 
-        -- check that head is only node in list
-        if previous == current then
-            self.head = nil
-        else
-            previous.next = nil
+        function list:popTail()
+            local previous = self.head
+            local current = previous
+
+            while current.next ~= nil do
+                previous = current
+                current = current.next
+            end
+
+            -- check that head is only node in list
+            if previous == current then
+                self.head = nil
+            else
+                previous.next = nil
+            end
+
+            return current
         end
 
-        return current
-    end
-
-    function list:pushTail(node)
-        if self.head == nil then
-            self.head = node
-        else
-            local tail = self:findTail()
-            tail.next = node
+        function list:pushTail(node)
+            if self.head == nil then
+                self.head = node
+            else
+                local tail = self:findTail()
+                tail.next = node
+            end
         end
-    end
 
-    function list:pushHead(node)
-        if self.head == nil then
-            self.head = node
-        else
-            node.next = self.head
-            self.head = node
+        function list:pushHead(node)
+            if self.head == nil then
+                self.head = node
+            else
+                node.next = self.head
+                self.head = node
+            end
         end
-    end
-
     return list
 end
 
--- test/list_test.lua
+++ test/list_test.lua
@@ -1,6 +1,28 @@
 local lu = require("luaunit")
-local list = rquire("list")
+local list = require("list")
 
+testNew = {}
+    function testNew:testHeadlessList()
+        l = list.new()
+        lu.assertEquals(l.head, nil)
+    end
 
+    function testNew:testHeadInitList()
+        l = list.new(1, 1)
+        lu.assertEquals(l.head, { x=1, y=1 })
+    end
+-- end testNew
+
+testListMethods = {}
+    function testListMethods:testSetHead()
+        l = list.new()
+        l:setHead(1, 2)
+        lu.assertEquals(l.head, { x=1, y=2 })
+
+        l:setHead(2, 2)
+        lu.assertEquals(l.head, { x=2, y=2 })
+
+    end
+-- end testListMethods
 
 os.exit(lu.LuaUnit.run())
