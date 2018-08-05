3a008eab0afc19479fe7dd08a766bb944c7111eb
Christopher Linton <cjlinton83@gmail.com>
2018-08-05 06:39:27 -0500

    list_test added testNew

--- src/list.lua
+++ src/list.lua
@@ -1,3 +1,17 @@
-list = {}
+local new = function(x, y)
+    local initHead = function(list)
+        if not(x and y) then
+            list.head = {}
+        else
+            list.head = { x=x, y=y }
+        end
+    end
 
-return list
+    list = {}
+        initHead(list)
+    return list
+end
+
+return {
+    new = new
+}
--- src/list_test.lua
+++ src/list_test.lua
@@ -5,4 +5,20 @@ function testListIsTable()
     lu.assertIsTable(list)
 end
 
+function testNewHead()
+    local l = list.new()
+    lu.assertEquals(l.head, {})
+
+    l = list.new(1, 1)
+    lu.assertEquals(l.head, { x=1, y=1 })
+end
+
+function testNewInstances()
+    local l1 = list.new()
+    lu.assertEquals(l1.head, {})
+
+    local l2 = list.new(2, 2)
+    lu.assertEquals(l2.head, { x=2, y=2 })
+end
+
 os.exit(lu.LuaUnit.run())
