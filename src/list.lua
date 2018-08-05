f8f43463c6a5db829268150efb6575c5f347fc87
Christopher Linton <cjlinton83@gmail.com>
2018-08-05 08:02:11 -0500

    additional test

--- src/list.lua
+++ src/list.lua
@@ -1,13 +1,40 @@
 local new = function(x, y)
     local initHead = function(list)
         if not(x and y) then
-            list.head = {}
+            list.head = nil
         else
-            list.head = { x=x, y=y }
+            list.head = list:newNode(x, y)
         end
     end
 
     list = {}
+        function list:newNode(x, y)
+            return { x=x, y=y }
+        end
+
+        function list:findTail()
+            local current = self.head
+
+            if not current then
+                return nil
+            end
+
+            while current.next ~= nil do
+                current = current.next
+            end
+
+            return current
+        end
+
+        function list:pushTail(node)
+            if not self.head then
+                self.head = node
+            else
+                local tail = self:findTail()
+                tail.next = node
+            end
+        end
+
         initHead(list)
     return list
 end
--- src/list_test.lua
+++ src/list_test.lua
@@ -5,20 +5,52 @@ function testListIsTable()
     lu.assertIsTable(list)
 end
 
-function testNewHead()
-    local l = list.new()
-    lu.assertEquals(l.head, {})
+testNew = {}
+    -- Main data of list is head plus associated methods
+    function testNewListInstances()
+        local l1 = list.new()
+        local l2 = list.new(2, 2)
 
-    l = list.new(1, 1)
-    lu.assertEquals(l.head, { x=1, y=1 })
-end
+        lu.assertEquals(l1.head, nil)
+        lu.assertEquals(l2.head, { x=2, y=2 })
 
-function testNewInstances()
-    local l1 = list.new()
-    lu.assertEquals(l1.head, {})
+        l1 = list.new(3, 3)
+        l2 = list.new(4, 4)
 
-    local l2 = list.new(2, 2)
-    lu.assertEquals(l2.head, { x=2, y=2 })
-end
+        lu.assertEquals(l1.head, { x=3, y=3 })
+        lu.assertEquals(l2.head, { x=4, y=4 })
+    end
+
+    function testNewNode()
+        local l = list.new()
+        local node = l:newNode(3, 3)
+
+        lu.assertEquals(l.head, nil)
+        lu.assertEquals(node, { x=3, y=3 })
+        lu.assertEquals(l.head, nil)
+    end
+-- end testNew
+
+testListMethods = {}
+    function testFindTail()
+        local l = list.new(1, 1)
+        local tail = l:findTail()
+     
+        lu.assertEquals(tail, { x=1, y=1 })
+
+        l = list.new()
+        tail = l:findTail()
+     
+        lu.assertEquals(tail, nil)
+    end
+
+    function testPushTail()
+        local l = list.new()
+        l:pushTail(l:newNode(1, 1))
+
+        lu.assertEquals(l.head, { x=1, y=1 })
+        lu.assertEquals(l:findTail(), { x=1, y=1})
+    end
+-- end testListMethods
 
 os.exit(lu.LuaUnit.run())
