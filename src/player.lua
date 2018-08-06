ca7d012917ef98998bf46427154613628c7033f8
Christopher Linton <cjlinton83@gmail.com>
2018-08-05 23:24:03 -0500

    newPlayer and test

--- src/player.lua
+++ src/player.lua
@@ -1,10 +1,21 @@
-local init = function()
-    local player = {}
-    return player
-end
+local new = function(color, count, x, y)
+    local initBody = function()
+        list = require("list")
+        body = list.new(x, y)
 
-local new = function()
-    return init()
+        for i=1, count-1 do
+            segment = body:newNode(x-i, y)
+            body:pushTail(segment)
+        end
+
+        return body
+    end
+    
+    player = {}
+        player.color = color
+        player.count = count
+        player.body = initBody()
+    return player
 end
 
 return {
--- src/player_test.lua
+++ src/player_test.lua
@@ -1,3 +1,21 @@
 local lu = require("luaunit")
+local player = require("player")
+
+function testPlayerIsTable()
+    lu.assertTable(player)
+end
+
+function testPlayerNewMultiIstance()
+    local p1 = player.new({ r=1, g=1, b=1 }, 3, 5, 5)
+    local p2 = player.new({ r=1, g=0, b=0 }, 2, 10, 10)
+
+    lu.assertEquals(p1.color, { r=1, g=1, b=1 })
+    lu.assertEquals(p1.count, 3)
+    lu.assertEquals(p1.body.head, { x=5, y=5, next={ x=4, y=5, next={ x=3, y=5 }}})
+
+    lu.assertEquals(p2.color, { r=1, g=0, b=0 })
+    lu.assertEquals(p2.count, 2)
+    lu.assertEquals(p2.body.head, { x=10, y=10, next={ x=9, y=10 }})
+end
 
 os.exit(lu.LuaUnit.run())
