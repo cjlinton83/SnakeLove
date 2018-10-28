b8d1a4ed051a964a16db1b530c14f9aab467029f
Christopher Linton <cjlinton83@gmail.com>
2018-10-28 00:49:27 -0500

    >>

---
+++ game.1.lua
@@ -0,0 +1,143 @@
+local new = function(width, height)
+    local game = {}
+        -- setup
+        game.cellSize = 20
+        game.columns = width/game.cellSize - 1
+        game.rows = height/game.cellSize - 1
+        game.cellCount = game.columns * game.rows - (2 * game.columns)  -- minus header cells
+        game.refreshRate = 0.07
+        game.quit = false
+
+        -- game state
+        game.gameOver = true
+        game.gameStart = false
+        game.gamePlay = false
+        
+        -- player state
+        game.currentPlayer = 1
+        game.playerCount = 1
+
+        function game:initPlayState()
+            local initPlayer = function(columns, rows)
+                local player = require("player")
+                local x = math.floor(columns/2)
+                local y = math.floor(rows/2)
+                local color = { r = 1, g = 1, b = 1 }
+                local bodySegmentCount = 3
+                local direction = "right"
+                return player.new(color, bodySegmentCount, x, y, direction)
+            end
+
+            self.playerTable = {}
+            for i = 1, self.playerCount do
+                self.playerTable[i] = initPlayer(self.columns, self.rows)
+            end
+
+            self.sumDT = 0
+            self.player = self.playerTable[self.currentPlayer]
+            self.food = { color = { r = 1, g = 0, b = 0 } }
+
+            self.gameOver = false
+            self.gameStart = true
+        end
+
+        function game:processInput(key)
+            local processInputGameOver = function(key)
+                if key == "return" then
+                    self:initPlayState()
+                end
+                if key == "up" or key == "down" then
+                    if self.playerCount == 1 then
+                        self.playerCount = 2
+                    else
+                        self.playerCount = 1
+                    end
+                end
+            end
+
+            local processInputPlayState = function(key)
+                local direction = self.player.direction
+
+                if self.player.updated then
+                    self.player.updated = false
+                    
+                    if key == "up" or key == "down" then
+                        if direction == "right" or direction == "left" then
+                            self.player.direction = key
+                        end
+                    end
+                    if key == "left" or key == "right" then
+                        if direction == "up" or direction == "down" then
+                            self.player.direction = key
+                        end
+                    end
+                end
+            end
+ 
+            if self.gameOver then
+                processInputGameOver(key)
+            else
+                if self.gameStart then
+                    if key then self.gameStart = false end
+                else
+                    processInputPlayState(key)
+                end
+            end
+
+            if key == "escape" then self.quit = true end
+        end
+
+        function game:processUpdate(dt)
+            local processUpdatePlayState = function(dt)
+                local updatePlayer = function()
+                end
+
+                local updateFood = function()
+                end
+
+                local handleGameOver = function()
+                end
+
+                self.sumDT = self.sumDT + dt
+    
+                if self.sumDT >= self.refreshRate then
+                    self.sumDT = self.sumDT - self.refreshRate
+
+                    updatePlayer()
+                    updateFood()
+                    handleGameOver()
+                end
+            end
+
+            if not self.gameOver and not self.gameStart then
+                processUpdatePlayState(dt)
+            end
+        end
+
+        function game:hasEmptyLocation()
+            if self.player.bodySegmentCount >= self.cellCount then
+                return false
+            end
+            return true
+        end
+
+        function game:getEmptyLocation()
+            local getLocation = function()
+                local x = math.random(0, self.columns)
+                local y = math.random(2, self.rows)
+                return { x=x, y=y }
+            end
+
+            local location = getLocation()
+            while self.player:containsLocation(location) do
+                location = getLocation()
+            end
+
+            return location
+        end
+    return game
+end
+
+return {
+    new = new
+}
